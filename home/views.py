a607046a4dfdb385313329d6bf79cf0c98de88c0
Jorge Escobar <j@jorge3.com>
2021-03-26 17:45:25 -0400

    finished step-3

--- home/views.py
+++ home/views.py
@@ -1,31 +1,16 @@
 from quart import (
     Blueprint,
     render_template,
-    request,
     session,
-    make_response,
     current_app,
     websocket,
 )
 from typing import Union, TYPE_CHECKING
 import uuid
 import asyncio
-import arrow
-import json
-import logging
 
 from user.decorators import login_required
 
-# from post.models import (
-#     get_post_parent_uid,
-#     get_last_feed_id,
-#     ActionType,
-#     get_latest_posts,
-#     get_post_comments,
-#     get_post_likes,
-#     get_post_uid_from_id,
-# )
-# from user.models import user_table, image_url_from_image_ts
 
 if TYPE_CHECKING:
     from quart.wrappers.response import Response
